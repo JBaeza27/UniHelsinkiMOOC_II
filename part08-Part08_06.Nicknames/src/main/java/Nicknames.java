0b9bb1ee9fdc3e2cd5f1bacc8c3b44a20a802de6
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2025-05-22 19:07:43 -0500

    completed part 8. challenge 6.

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,13 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String, String> nicknameMap = new HashMap<>();
+        
+        nicknameMap.put("matthew","matt");
+        nicknameMap.put("michael","mix");
+        nicknameMap.put("arthur","artie");
+        
+        System.out.println(nicknameMap.get("matthew"));
     }
 
 }
