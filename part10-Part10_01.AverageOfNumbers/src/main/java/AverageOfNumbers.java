7fb529cfb320c2696dabecda1cdcfb8243575464
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2025-08-11 11:32:04 -0500

    completed part 10. challenge 1.

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -7,6 +7,26 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your program here
-
+        ArrayList<String> inputs = new ArrayList<>();
+        
+         System.out.println("Input numbers, type \"end\" to stop");
+        while(true){
+            
+           
+            String row = scanner.next();
+            if(row.equals("end")){
+                break;
+            }
+            
+            inputs.add(row);
+            
+        }
+        
+        double average = inputs.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .average()
+                .getAsDouble();
+        
+        System.out.println("Average of the numbers: " + average);
     }
 }
