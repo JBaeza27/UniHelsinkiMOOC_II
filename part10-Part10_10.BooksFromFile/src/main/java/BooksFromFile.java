d2d627bf1adfa1a4eca79d2b55d060b1965a5991
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2025-08-14 16:16:52 -0500

    completed part 10. challenge 10 and 11.

--- part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -11,7 +11,23 @@ public class BooksFromFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
-
+        
+    }
+    
+    public static List<Book> readBooks(String file){
+        ArrayList<Book> contents = new ArrayList<>();
+        try{
+            Files.lines(Paths.get(file))
+                    .map(i -> i.split(","))
+                    .map(parts -> new Book(parts[0], Integer.valueOf(parts[1]), Integer.valueOf(parts[2]), parts[3]))
+                    .forEach(content -> contents.add(content));
+            //collect(Collectors.toList());
+        }catch(Exception E){
+            System.out.println("Error: " + E.getMessage());
+        }
+        //return new ArrayList<>();
+        return contents;
+        
     }
 
 }
--- part10-Part10_11.WageOrder/src/main/java/Human.java
+++ part10-Part10_11.WageOrder/src/main/java/Human.java
@@ -1,5 +1,5 @@
 
-public class Human {
+public class Human implements Comparable<Human>{
 
     private int wage;
     private String name;
@@ -16,8 +16,19 @@ public class Human {
     public int getWage() {
         return wage;
     }
-
-
+    
+    
+    public int compareTo(Human human){
+        // return another.getWage() - this.getWage();
+        if (this.wage == human.getWage()) {
+            return 0;
+        } else if (this.wage < human.getWage()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    
     @Override
     public String toString() {
         return name + " " + wage;
