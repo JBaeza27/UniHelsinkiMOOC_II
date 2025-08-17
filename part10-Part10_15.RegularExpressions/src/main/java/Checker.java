d45965ea79132f32832dfd385eb03180657726a0
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2025-08-17 17:17:18 -0500

    completed part 10. challenge 15.

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,27 @@
 
-
 public class Checker {
+
+    public boolean isDayOfWeek(String string) {
+        if (string.matches("mon") || string.matches("tue") || string.matches("wed")
+                || string.matches("thu") || string.matches("fri")
+                || string.matches("sat") || string.matches("sun")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean allVowels(String string) {
+        if (string.matches("[aeiou]*")) {
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean timeOfDay(String string){
+        if (string.matches("([01][0123456789]|2[0123]):[012345][0123456789]:[012345][0123456789]")) {
+            return true;
+        }
+        return false;
+    }
 
 }
--- part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -5,5 +5,13 @@ public class MainProgram {
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        Checker check = new Checker();
+        
+        String str = "fri";
+        System.out.println(check.isDayOfWeek(str));
+        String str2 = "aeaeeaeiiwew";
+        System.out.println(check.allVowels(str2));
+         String str3 = "10:10:10";
+        System.out.println(check.isDayOfWeek(str3));
     }
 }
