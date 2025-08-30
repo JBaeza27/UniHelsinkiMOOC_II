82dcd0235dad34dd9a22f2a53122c46aa87388a5
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2025-08-30 09:28:42 -0500

    completed part 11. challenge 4,5,6, and 7.

---
+++ part11-Part11_06.SaveablePerson/src/main/java/Saveable.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sierra02
+ */
+public interface Saveable {
+//    public void save();
+//    public void delete();
+//    public void load(String address); --- instead of this do
+
+    void save();
+
+    void delete();
+
+    void load(String address);
+}
