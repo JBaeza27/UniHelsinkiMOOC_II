7d151471143fd7c797d28951497de04db57f16ef
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2026-01-23 20:58:55 -0600

    completed part 12. challenge 8.

--- part12-Part12_08.Lottery/src/main/java/LotteryRow.java
+++ part12-Part12_08.Lottery/src/main/java/LotteryRow.java
@@ -19,12 +19,23 @@ public class LotteryRow {
         // Initialize the list for numbers
         this.numbers = new ArrayList<>();
         // Implement the random number generation here
+        Random random = new Random();
+        for(int i = 0; i < 7; i++){
+            int numberDrawn = 1 + random.nextInt(40);
+            // while the number is in the array list
+            while(containsNumber(numberDrawn)){
+                numberDrawn = 1 + random.nextInt(40);
+            }
+            this.numbers.add(numberDrawn);
+        }
         // the method containsNumber is probably useful
+        
+        
     }
 
     public boolean containsNumber(int number) {
         // Check here whether the number is among the drawn numbers
-        return false;
+        return this.numbers.contains(number);
     }
 }
-
+
